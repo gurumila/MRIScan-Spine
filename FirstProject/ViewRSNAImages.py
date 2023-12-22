c774215eb6e46b939b6bc819c061057491cf819a
gurumila <gurumila22@gmail.com>
2023-12-22 20:58:21 +0530

    Sample to view a slice from .nii file

--- FirstProject/ViewRSNAImages.py
+++ FirstProject/ViewRSNAImages.py
@@ -6,16 +6,17 @@ Created on Wed Dec 20 23:31:33 2023
 @author: milan
 """
 
-import nifti_reader as nr
-from PIL import Image
-from pylab import *
+import nibabel as nib
+#import nifti_reader as nr
+#from PIL import Image
+from pylab import plt
 
 
 #nr.read_nifti_file('../RSNA/data/train_images/1.2.826.0.1.3680043.14/1.dcm')
 
 #nr.read_nifti_file('../RSNA/data/segmentations/1.2.826.0.1.3680043.780.nii')
 
-volume = nr.process_scan('../RSNA/data/segmentations/1.2.826.0.1.3680043.780.nii')
+#volume = nr.process_scan('../RSNA/data/segmentations/1.2.826.0.1.3680043.780.nii')
 
 
 #im = array(Image.open('images/profile.jpg').convert('L'))
@@ -29,14 +30,14 @@ volume = nr.process_scan('../RSNA/data/segmentations/1.2.826.0.1.3680043.780.nii
 #https://nipy.org/nibabel/reference/nibabel.html
 
 # This does not work
-imshow(volume)
-show();
+#imshow(volume)
+#show();
 
 
 ##########
 # TRY THIS
 # Load the NIfTI file
-file_path = 'path/to/your/file.nii'
+file_path = '../RSNA/data/segmentations/1.2.826.0.1.3680043.780.nii'
 img = nib.load(file_path)
 
 # Get the data array
@@ -46,7 +47,7 @@ data = img.get_fdata()
 num_slices = data.shape[-1]
 
 # You can change the slice index to view different slices
-slice_index = num_slices // 2
+slice_index = 18 # // 2
 
 # Display the slice using matplotlib
 plt.imshow(data[:, :, slice_index], cmap='gray')
